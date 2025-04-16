d7b8b52615c5a1a8e3c36f5a8dab6b1c58dd3b18
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-04-16 12:33:52 +0200

    tp1 finished

---
+++ app/add_user.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'includes/dbconnect.php';
+
+$username = 'makombela';
+$email = 'makombela@test.cd';
+
+
+$add_user = 'INSERT INTO users(username, email) VALUES(:username, :email)';
+
+
+$stmt = $pdo->prepare($add_user);
+$exec = $stmt->execute([
+    'username' => $username,
+    'email' => $email
+]);
+if($exec){
+    echo '<p>Inscription réussie</p>';
+}
